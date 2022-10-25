0eac2de9a43514aade803cdf42ecdedec19b78dc
= <rakib.ivan100@gmail.com>
2022-10-25 20:07:29 +0600

    dynamic api created

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors())
 
 const courses = require('./data/courses.json')
+
 app.get('/', (req, res) => {
     res.send('api running')
 })
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const getSingleCourse = courses?.find(p => p.id == id)
+    res.send(getSingleCourse);
 })
 
 app.listen(port, () => {
